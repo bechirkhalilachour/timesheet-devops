5241b9a287b288729d664697e2f27610fc5dc803
khalil achour <Bechirkhalil.achour@esprit.tn>
2021-10-20 02:15:37 +0100

    initialisation du projet timesheet-devops

---
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -0,0 +1,74 @@
+package tn.esprit.spring.services;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.entities.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+class UserServiceImplTest {
+	
+	@Autowired 
+	IUserService us;
+	
+	
+	@Test
+	@Order(1)
+	public void testRetrieveAllUsers() {
+		List<User> listUsers =us.retrieveAllUsers();
+		Assertions.assertEquals(4, listUsers.size());
+	}
+	
+	@Test
+ 	@Order(2)
+	public void testAddUser() throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date d = dateFormat.parse("1993-01-23");
+		User user = new User("Hajer", "Jileni", d , Role.INGENIEUR);
+		User userAdd = us.addUser(user);
+		Assertions.assertEquals(user.getLastName(), userAdd.getLastName());
+	}
+	
+	@Test
+ 	@Order(3)
+	public void testUpdateUser() throws ParseException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date d = dateFormat.parse("1965-03-23");
+		User user = new User(1L,"Jhony", "Deppss", d , Role.INGENIEUR);
+		User userUpdated = us.addUser(user);
+		Assertions.assertEquals(user.getLastName(), userUpdated.getLastName());
+	}
+ 	
+ 	@Test
+ 	@Order(4)
+	public void testRetrieveUser() throws ParseException {
+		User userRetrieved = us.retrieveUser("2");
+		Assertions.assertEquals(1L, userRetrieved.getId());
+	}
+ 	
+	@Test
+ 	@Order(5)
+	public void testDeleteUser() throws ParseException {
+		us.deleteUser("4");;
+		Assertions.assertNull(us.retrieveUser("4"));
+	}
+	
+
+ 	
+ 	
+	
+	 
+}
